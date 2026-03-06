2cf7c7e8684397f9c4e2a56f0cd2cc16f9238c5c
joviprata <joviprata07@gmail.com>
2026-03-06 15:30:02 -0300

    changes up to video 29 (image logic)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -15,5 +15,9 @@ export class UserComponent {
   onSelectedUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     this.selectedUser = DUMMY_USERS[randomIndex];
+  }
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
   }
 }
