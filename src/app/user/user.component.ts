f547839a462cdf5581d1d75721275019cc1bccdc
joviprata <joviprata07@gmail.com>
2026-03-06 18:14:28 -0300

    signals (changes after video 29)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,14 +10,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectedUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
-
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
